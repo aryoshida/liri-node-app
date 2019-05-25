a73551ad7e8fdb068712a7a91d7d22dd813abd9f
Andrea Yoshida <aryoshida9340@gmail.com>
2019-05-24 21:25:25 -0600

    DONE

--- keys.js
+++ keys.js
@@ -5,5 +5,4 @@ exports.spotify = {
   secret: process.env.SPOTIFY_SECRET
 };
 
-// Client ID 78c3bd5485e9438f944b4cd11985965c
-// Client Secret e761e1a1836e413d90a64e0d5a73b99e 
+
--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const fs = require("fs");
 const keys = require("./keys.js"); 
+const Spotify = require("node-spotify-api");
 const spotify = new Spotify(keys.spotify);
 const axios = require("axios");
 
@@ -8,16 +9,22 @@ var command = process.argv[2];
 var value = process.argv[3];
 
 if(command === "concert-this"){
-    concertSearch();
-
+    concertSearch(value);
+    if(!value) {
+        value = "jon bellion";
+    }
 } else if(command === "spotify-this-song"){
-    songSearch();
-
+    songSearch(value);
+    if(!value) {
+        value = "Never Say Never";
+    }
 } else if(command === "movie-this"){
-    movieSearch();
-
-} else{
-
+    if(!value) {
+        value = "Mr. Nobody";
+    }
+    movieSearch(value);
+} else if(command === "do-what-it-says"){
+    doWhatItSays();
 }
 
 function concertSearch(value){
@@ -26,14 +33,15 @@ function concertSearch(value){
         // Name of the venue
         // Venue location
         // Date of the Event (use moment to format this as "MM/DD/YYYY")
-    var URL = ""
+    var URL = "https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp";
     axios.get(URL).then(function(response){
-        var jsonData = response.data;
+        var jsonData = response.data[0];
         var concertData = [
-            "Venue Name: " + jsonData.
-            "Venue Location: " = jsonData.
-            "Event Date: " = jsonData.
-        ].join("\n\n)";
+            "Venue Name: " + jsonData.venue.name,
+            "Venue Location: " + jsonData.venue.country + " " + jsonData.venue.city,
+            "Event Date: " + jsonData.datetime
+        ].join("\n\n");
+        console.log(concertData);
     });
 }
 
@@ -43,15 +51,27 @@ function songSearch(value){
         // The song's name
         // A preview link of the song from Spotify
         // The album that the song is from
-    var URL = ""
-    axios.get(URL).then(function(response){
-        var jsonData = response.data;
-        var songData = [
-            "Artist(s): " + jsonData.
-            "Song Name: " + jsonData.
-            "Link to Song: " + jsonData.
-            "Album: " + jsonData.
-        ].join("\n\n");
+    
+    spotify.search({ type: "track", query: value }, function(err, data){
+        if(err){
+            return console.log("error: " + err);
+        }
+
+        var artists = data.tracks.items[0].artists;
+        console.log("Artist for this track: ");
+        for(var i = 0; i < artists.length; i++){
+            console.log("\t" + artists[i].name);
+        }
+
+        console.log("Song Name: ");
+        console.log("\t" + value);
+
+        console.log("Preview URL: ");
+        console.log("\t" + data.tracks.items[0].preview_url);
+        
+        console.log("Album Name: ")
+        console.log("\t" + data.tracks.items[0].album.name);
+        
     });
 }
 
@@ -65,23 +85,41 @@ function movieSearch(value){
         // Language of the movie.
         // Plot of the movie.
         // Actors in the movie
-    var URL = ""
+
+    var URL = "http://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=trilogy";
     axios.get(URL).then(function(response){
         var jsonData = response.data;
         var movieData = [
-            "Title: " + jsonData.
-            "Release Year: " + jsonData.
-            "IMDB Rating: " + jsonData.
-            "Rotten Tomatoes Rating: " + jsonData.
-            "Country: " + jsonData.
-            "Language: " + jsonData.
-            "Movie Plot: " + jsonData.
-            "Actors: " + jsonData.
+            "Title: " + value,
+            "Release Year: " + jsonData.Released,
+            "IMDB Rating: " + jsonData.imdbRating,
+            "Rotten Tomatoes Rating: " + getRottenTomatoesRating(jsonData.Ratings),
+            "Country: " + jsonData.Country,
+            "Language: " + jsonData.Language,
+            "Movie Plot: " + jsonData.Plot,
+            "Actors: " + jsonData.Actors
         ].join("\n\n");
+        console.log(movieData);
     });
 }
 
+function getRottenTomatoesRating(ratings) {
+    for(var i = 0; i < ratings.length; i++){
+        if(ratings[i].Source === "Rotten Tomatoes"){
+            return ratings[i].Value;
+        } 
+    } return "N/A";
+}
 
-    // do-what-it-says
-        // It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
-        // Edit the text in random.txt to test out the feature for movie-this and concert-this.
+function doWhatItSays(){
+    fs.readFile("random.txt", "utf8", function(err, data){
+        if(err){
+            return console.log(err);
+        }
+        var dataArray = data.split(",");
+        console.log(data);
+        var command = dataArray[0];
+        var value = dataArray[1];
+        songSearch(value);
+    });
+}
